92ce8a9be6f6e34e243c7262c11905728032cceb
Yousaf Syed <mmesunny@gmail.com>
2016-06-03 22:56:51 +0200

    Updates

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_USERNAME',null), 'name' => env('MAIL_NAME',null)],
 
     /*
     |--------------------------------------------------------------------------
